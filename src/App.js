8f39a317839c4e8e54b2133f20ad7e5634338841
Claire List <claire.list2501@gmail.com>
2021-11-30 14:38:17 -0700

    doing the most on these spans

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    <div>
+      <h1>GIT HUB INFORMATIC</h1>
+      <div className='github-handle'>$GITHUB-HANDLE 
+      <span>
+        <form>
+          <input placeholder='S-E-A-R-C-H'/>
+          <button>LOOKUP a USER.</button>
+        </form>
+      </span>
+      </div>
+
+      <div className='user-infos'>
+        <img src='' alt='user $GETUSERAVI'></img>
+        <span>-     Full Nahme</span>
+        <p>username $FROM_USERNAME</p>
+        <p>TOTAL REPOS: $NUMREPOS</p>
+        <p>TOTAL FOLLOWERS: $NUMFOLLOWERS</p>
+      </div>
+
+      <div className='followers-array'>FOLLOWERS FOLLOWERS FOLLOWERS</div>
+
+    </div>
+    );
   }
 }
 
